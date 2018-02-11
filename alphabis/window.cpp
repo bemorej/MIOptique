b09bff3b6d88bfed7a4d445ef40ce43299d43202
bemorej <jeromebouton@protonmail.com>
2018-02-11 19:57:52 +0100

    ajout group box

--- alphabis/window.cpp
+++ alphabis/window.cpp
@@ -6,6 +6,11 @@
 #include <QTextEdit>
 #include <QFileDialog>
 #include <QDebug>
+#include <QLineEdit>
+#include <QGroupBox>
+#include <QLabel>
+#include <QComboBox>
+#include <QGridLayout>
 void Window::afficher_window2()
 {
 	//Window2 *window = new Window2(this);
@@ -35,8 +40,37 @@ Window::Window(QWidget *parent) :
 	writeintofiletoto = new QPushButton("Write", this);
 	int wbutton = 80, hbutton = 30;
 	int xbutton = (width-wbutton)/2, ybutton = (height-hbutton)/2;
+	//
+	//QLineEdit *edit = new QLineEdit("");
+
+	
 	afficherwindow2->setGeometry(xbutton, ybutton, wbutton, hbutton);
 	writeintofiletoto->setGeometry(xbutton+50, ybutton, wbutton, hbutton);
+	/* */
+	QGroupBox *echoGroup = new QGroupBox(tr("Ying"),this);
+
+	//QLabel *echoLabel = new QLabel(tr("Mode:"),this);
+	//QComboBox *echoComboBox = new QComboBox;
+	//echoComboBox->addItem(tr("Normal"));
+	//echoComboBox->addItem(tr("Password"));
+	//echoComboBox->addItem(tr("PasswordEchoOnEdit"));
+	//echoComboBox->addItem(tr("No Echo"));
+
+	echoLineEdit = new QLineEdit(this);
+	echoLineEdit->setPlaceholderText("Placeholder Text");
+	echoLineEdit->setFocus();
+
+	echoLineEdit->text();
+	//echoLineEdit->setGeometry(,,20,20);
+
+	QGridLayout *echoLayout = new QGridLayout;
+	echoLayout->addWidget(echoLineEdit, 0, 0);
+	//echoLayout->addWidget(echoComboBox, 0, 1);
+
+	echoGroup->setLayout(echoLayout);
+	/* */
+
+
 	QObject::connect(afficherwindow2, SIGNAL(clicked()), this, SLOT(afficher_window2()));
 	QObject::connect(writeintofiletoto, SIGNAL(clicked()), this, SLOT(writeintofile()));
 }
--- alphabis/window.h
+++ alphabis/window.h
@@ -2,18 +2,22 @@
 #define WINDOW_H
 #include "window2.h"
 #include <QWidget>
+#include <QLineEdit>
 class QPushButton;
 class Window : public QWidget
 {
 	Q_OBJECT
 	public:
 		explicit Window(QWidget *parent = 0);
-		public slots:
-			void afficher_window2();
-			void writeintofile();
+
+	public slots:
+		void afficher_window2();
+		void writeintofile();
+
 	private:
 		QPushButton *afficherwindow2;
 		QPushButton *writeintofiletoto;
 		Window2 *fenetre;
+		QLineEdit *echoLineEdit;
 };
 #endif // WINDOW_H
